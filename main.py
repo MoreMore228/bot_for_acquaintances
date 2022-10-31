34de631332680cd75b29fc1133e548a01101f137
Михаил2403 <mihailpickalev@gmail.com>
2022-10-31 14:19:19 +0500

    h

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def start(message):
     # #user_name and user_id will need to be entered into the database
     config.temporary_storage_of_received_data[message.from_user.id] = {}
     config.temporary_storage_of_received_data[message.from_user.id]['user_id'] = message.from_user.id
-<<<<<<< HEAD
     try:
         if config.temporary_storage_of_received_data[message.from_user.id]["user_id"] == db.search_tuple_db(message.from_use.id)["user_id"]:   #Здесь будет проверка наличия анкеты у пользователя
             bot.send_message(message.chat.id, "Хотите перейти к просмотру анкет?", reply_markup=kb.menu_kb())
@@ -24,17 +23,15 @@ def start(message):
             start = bot.send_message(message.chat.id, "Создадим анкету (отправьте что угодно, чтобы начать)", reply_markup=kb.reg_kb())
             bot.register_next_step_handler(start, reg)
     except TypeError:
-=======
     
-    if config.temporary_storage_of_received_data[message.from_user.id]["user_id"] == db.search_tuple_db(message.from_user.id)["user_id"]:   #Здесь будет проверка наличия анкеты у пользователя
-        bot.send_message(message.chat.id, "Хотите перейти к просмотру анкет?", reply_markup=kb.menu_kb())
-    else:   #добавляем user_id, user_name в бд
->>>>>>> 1d2b56e36af48e4c0aa0681d852f4d0ed4e442f0
-        config.temporary_storage_of_received_data[message.from_user.id]["user_name"] = str("@" + message.from_user.username)
-            #    ...
-            #     И переходим к регистрации, добавляя вводимые данные в бд
-        start = bot.send_message(message.chat.id, "Создадим анкету (отправьте что угодно, чтобы начать)", reply_markup=kb.reg_kb())
-        bot.register_next_step_handler(start, reg)
+        if config.temporary_storage_of_received_data[message.from_user.id]["user_id"] == db.search_tuple_db(message.from_user.id)["user_id"]:   #Здесь будет проверка наличия анкеты у пользователя
+            bot.send_message(message.chat.id, "Хотите перейти к просмотру анкет?", reply_markup=kb.menu_kb())
+        else:   #добавляем user_id, user_name в бд
+            config.temporary_storage_of_received_data[message.from_user.id]["user_name"] = str("@" + message.from_user.username)
+                #    ...
+                #     И переходим к регистрации, добавляя вводимые данные в бд
+            start = bot.send_message(message.chat.id, "Создадим анкету (отправьте что угодно, чтобы начать)", reply_markup=kb.reg_kb())
+            bot.register_next_step_handler(start, reg)
 
 def reg(message):
     #Запрос пола
